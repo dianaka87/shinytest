9cad9fc4230500aac047b7e54df952b984dff5d6
dianaka87 <55535284+dianaka87@users.noreply.github.com>
2021-12-21 09:11:00 +0000

    Test2

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(ggplot2)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Brand Personality Profile edited DK"),
+    titlePanel("Brand Personality Profile edited DK 2"),
 
     sidebarLayout(
         # Sidebar with a drop-down menu to select a brand
